b96ad400d7e09d2e22aadb65b05ccb9501e71555
Zerra86 <111981309+Zerra86@users.noreply.github.com>
2022-09-22 16:08:51 -0300

    Add files via upload

---
+++ Atv1.ts
@@ -0,0 +1,17 @@
+class patinete {
+    name: string
+    age: number
+    apto: boolean
+    
+    sinopse(){
+        console.log(`Tenho o ${patin.name},a ${patin.age}anos`);
+    }
+}
+
+const patin = new patinete();
+patin.name = "etenitap";
+patin.age = 21;
+patin.apto = false;
+
+patin.sinopse();
+
